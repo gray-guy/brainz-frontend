5cd5c0444f527ab22783dcd66a1573299d83a9e3
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-08-12 18:52:26 +0500

    hide toc modal for the time being

--- app/contexts/UserContext.jsx
+++ app/contexts/UserContext.jsx
@@ -25,10 +25,10 @@ const UserProvider = ({ children }) => {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
-      <TermsConditionsModal
+      {/* <TermsConditionsModal
         isOpen={!!user && !user.hasAcceptedToc}
         onAccept={handleAccepToc}
-      />
+      /> */}
       <WelcomeModal showModal={!user && showWelcome} setShowModal={setShowWelcome} />
     </UserContext.Provider>
   );
