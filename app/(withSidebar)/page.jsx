da989ce726d767959880fecd483631066c12fd1f
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-08-16 11:20:06 +0500

    add stripe checkout form

--- app/(withSidebar)/page.jsx
+++ app/(withSidebar)/page.jsx
@@ -1,22 +1,32 @@
-"use client";
-import Dashboard from "@/app/container/Dashboard";
-import Loader from "../components/Loader";
-import { usePrivy } from "@privy-io/react-auth";
+"use client"
+import Dashboard from "@/app/container/Dashboard"
+import Loader from "../components/Loader"
+import { usePrivy } from "@privy-io/react-auth"
+import { loadStripe } from "@stripe/stripe-js"
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
 
 export default function Page() {
-  const { authenticated } = usePrivy();
+  const { authenticated } = usePrivy()
 
   if (authenticated) {
     return (
       <Loader>
         <Dashboard />
       </Loader>
-    );
+    )
   }
 
   return (
     <div>
       <Dashboard />
+
+      <form
+        action={`${process.env.NEXT_PUBLIC_API_URL}/create-checkout-session`}
+        method="POST"
+      >
+        <button type="submit">Checkout</button>
+      </form>
     </div>
-  );
+  )
 }
