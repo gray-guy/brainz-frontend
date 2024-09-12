ac51e5082b39e52b087ff2402ae60c97c0e1d2e0
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-09-12 15:45:01 +0500

    remove title and sessions in live game card

--- app/components/SessionCard.jsx
+++ app/components/SessionCard.jsx
@@ -8,11 +8,11 @@ const SessionCard = ({ game, activeIdx }) => {
 
   return (
     <div className="h-auto w-full rounded-[10px] bg-primary-100 px-4 py-4 text-base shadow-sessionCard md:py-7 lg:px-5 lg:py-5">
-      <h2 className="captilize font-grey-400 text-center font-basement text-lg font-bold md:text-left">
+      {/* <h2 className="captilize font-grey-400 text-center font-basement text-lg font-bold md:text-left">
         {title}
-      </h2>
-      <div className="relative flex gap-1 md:gap-2">
-        <div className="absolute left-0 z-[12] mt-2 max-w-full flex-1 lg:mt-6">
+      </h2> */}
+      {/* <div className="relative flex gap-1 md:gap-2"> */}
+      {/* <div className="absolute left-0 z-[12] mt-2 max-w-full flex-1 lg:mt-6">
           {sessions
             .slice(minSliceStart, minSliceStart + 3)
             .map((session, index) => (
@@ -25,19 +25,17 @@ const SessionCard = ({ game, activeIdx }) => {
                 {session.topic?.title ?? `Session ${index + 1}`}
               </h1>
             ))}
-        </div>
-        <div className="mt-2 flex min-h-44 flex-1 items-center justify-center lg:mt-6">
-          <div className="relative ml-auto h-full w-1/2">
-            <Image
-              src={image}
-              layout="fill"
-              objectFit="contain"
-              alt="Next Game"
-              draggable={false}
-            />
-          </div>
-        </div>
+        </div> */}
+      <div className="relative min-h-[230px]">
+        <Image
+          src={image}
+          layout="fill"
+          objectFit="cover"
+          alt="Next Game"
+          draggable={false}
+        />
       </div>
+      {/* </div> */}
     </div>
   )
 }
