e5fa6c1fd75452c3d0a91cc3dabb34a005e7b3f9
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-09-12 16:27:38 +0500

    update upcoming session ui

--- app/components/CryptoCard.jsx
+++ app/components/CryptoCard.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Button } from "./Button"
 import moment from "moment"
 
 const cardClasses = [
@@ -14,42 +13,31 @@ const CryptoCard = ({ idx, data }) => {
     <div
       className={`${cardClasses[idx % 3]} w-full rounded-[10px] px-4 py-4 lg:px-5 lg:py-5`}
     >
-      <p className="font-basement text-base font-bold">{title}</p>
-      <div className="flex">
-        <div className="mt-4 max-w-[70%] flex-1">
-          <div className="flex h-full flex-col overflow-hidden">
-            <ul className="flex list-disc flex-col gap-1">
-              {sessions.slice(0, 3).map((session, index) => {
-                return (
-                  <li
-                    key={index}
-                    className="list-disc truncate font-basement text-sm font-bold capitalize"
-                  >
-                    - {session.topic?.title ?? `Session ${index + 1}`}
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
-          <div className="mt-5">
-            {/* <Button
-              variant={"outlined"}
-              className={"pt-1 pb-1 px-3.5 text-nowrap "}
-              size="text-sm"
-            >
-              Remind me
-            </Button> */}
-          </div>
+      <div className="grid grid-cols-4 font-basement">
+        <div className="col-span-3 mr-3">
+          <p className="line-clamp-2 min-h-14 text-center text-lg font-bold md:text-xl">
+            {title}
+          </p>
+          <ul className="mt-3 list-disc gap-1 space-y-1">
+            {sessions.slice(0, 2).map((session, index) => (
+              <li
+                key={index}
+                className="list-disc truncate text-sm font-bold capitalize"
+              >
+                - {session.topic?.title ?? `Session ${index + 1}`}
+              </li>
+            ))}
+          </ul>
         </div>
 
-        <div className="items-right flex flex-1 flex-col gap-1.5 text-right">
-          <h1 className="font-basement text-lg font-bold text-white md:text-xl">
-            {moment(startTime).format("MMMM")}
+        <div className="space-y-2 text-center">
+          <h1 className="text-lg md:text-xl">
+            {moment(startTime).format("MMM")}
           </h1>
-          <h1 className="font-basement text-3xl font-[900] text-white lg:text-4xl">
+          <h1 className="text-3xl font-[900] lg:text-4xl">
             {moment(startTime).format("DD")}
           </h1>
-          <h1 className="font-basement text-lg font-bold text-white md:text-xl">
+          <h1 className="text-lg md:text-xl">
             {moment(startTime).format("h a")}
           </h1>
         </div>
