a9866d604b0f238587724e24dbf20967b7490feb
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-09-26 11:44:51 +0500

    move dashboard data fetching to server

--- app/(withSidebar)/page.jsx
+++ app/(withSidebar)/page.jsx
@@ -1,15 +1,66 @@
-"use client"
+import axios from "axios"
 import Dashboard from "@/app/container/Dashboard"
 import Loader from "../components/Loader"
-// import { loadStripe } from "@stripe/stripe-js"
 
-// const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
-export default function Page() {
+export default async function Page() {
+  const fetchedGames = await axios.get(`${BASE_URL}/games`)
+  const currentTime = new Date()
+  const validGames = fetchedGames.data.filter((game) => {
+    if (game.sessions.length === 0) return false
+    return currentTime < new Date(game.endTime)
+  })
+  validGames.sort((a, b) => new Date(a.endTime) - new Date(b.endTime))
+  let nextGame = null
+  let upcomingGames = []
+  if (validGames.length > 0) {
+    nextGame = validGames[0]
+    upcomingGames = validGames.slice(1)
+  }
+
+  let session = null
+  let sessionIdx = 0
+  if (nextGame) {
+    nextGame.sessions.forEach((session) => {
+      if (currentTime > new Date(session.endTime)) {
+        session.status = "completed"
+      } else if (currentTime < new Date(session.startTime)) {
+        session.status = "upcoming"
+      } else {
+        session.status = "live"
+      }
+    })
+
+    // TODO: what's the second check on length?
+    const findFunc = (item, idx, self) =>
+      item.status === "upcoming" || self.length - 1 === idx
+    sessionIdx = nextGame.sessions.findIndex(findFunc)
+    session = nextGame.sessions.find(findFunc)
+  }
+
+  let wheelRewards = null
+  if (session) {
+    try {
+      const wheelResp = await axios.get(
+        `${BASE_URL}/wheels/session/${session.id}?getOnlyRewards=true`
+      )
+      wheelRewards = wheelResp.data.totalRewards
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div>
       <Loader />
-      <Dashboard />
+      <Dashboard
+        nextGame={nextGame}
+        games={upcomingGames}
+        sessionIdx={sessionIdx}
+        session={session}
+        wheelRewards={wheelRewards}
+      />
     </div>
   )
 }
