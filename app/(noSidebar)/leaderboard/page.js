ac8c8b6e885c3fbd912cf2c05dc9bca7aa9065c7
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-10-02 11:20:22 +0500

    add bg image on leaderboard

--- app/(noSidebar)/leaderboard/page.js
+++ app/(noSidebar)/leaderboard/page.js
@@ -6,7 +6,10 @@ import { NextSessionCard } from "./next-session"
 export default function Page() {
   return (
     <div>
-      <div className="mx-auto mb-16 mt-20 max-w-[1100px]">
+      <div className="absolute -top-10 left-1/2 -translate-x-1/2 aspect-[1440/986] max-h-[900px] w-full min-w-[800px]">
+        <Image fill src="/images/leaderboard-bg.svg" alt="Leaderboard" />
+      </div>
+      <div className="relative mx-auto mb-16 mt-20 max-w-[1100px]">
         <div className="mb-14 grid grid-cols-2 gap-7">
           <RankCard />
           <NextSessionCard />
